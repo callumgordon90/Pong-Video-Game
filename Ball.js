9271027376954371b4f39f79643b62eda50f3d75
callumgordon90 <callumjosephiaingordon@gmail.com>
2022-06-23 18:36:11 +0200

    Ball.js 'reset' helper function to control trajectory of ball

--- Ball.js
+++ Ball.js
@@ -1,6 +1,7 @@
 export default class Ball {
     constructor(ballElem) {
         this.ballElem = ballElem
+        this.reset()
     }
 
     //x position
@@ -20,6 +21,16 @@ export default class Ball {
 
     set y(value){
         this.ballElem.style.setProperty("--y", value)
+    }
+
+    reset(){
+        this.x = 50 
+        this.y = 50
+        this.direction = { x: 0.75, y: 0.5 }
+        while (true) {
+            const heading = randomNumberBetween(0 , 2 * Math.PI)
+            this.direction = { x: Math.cos(heading), y: Math.sin(heading) }
+        }
     }
 
     update(delta) {
