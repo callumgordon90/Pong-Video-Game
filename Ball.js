877d4410d2329922d20ed0a80a8a9762598ec75b
callumgordon90 <callumjosephiaingordon@gmail.com>
2022-06-23 20:51:28 +0200

    functionality to update either the player score or the computer score when the ball goes out of bounds

--- Ball.js
+++ Ball.js
@@ -56,9 +56,9 @@ export default class Ball {
             this.direction.y *= -1
         }
 
-        if (rect.right >= window.innerWidth || rect.left <= 0) {
-            this.direction.x *= -1
-        }
+      //  if (rect.right >= window.innerWidth || rect.left <= 0) {
+      //      this.direction.x *= -1
+      //  }
     }
 }
 
