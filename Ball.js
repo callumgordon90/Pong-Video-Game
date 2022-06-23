170e3d92127532a7e136ef03379939d54f045a36
callumgordon90 <callumjosephiaingordon@gmail.com>
2022-06-23 18:27:33 +0200

    x and y position of ball set up , 5 and and 15

--- Ball.js
+++ Ball.js
@@ -2,4 +2,28 @@ export default class Ball {
     constructor(ballElem) {
         this.ballElem = ballElem
     }
+
+    //x position
+    get x() {
+        return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--x"))
+    }
+
+    set x(value){
+        this.ballElem.style.setProperty("--x", value)
+    }
+
+
+    //y position
+    get y() {
+        return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--y"))
+    }
+
+    set y(value){
+        this.ballElem.style.setProperty("--y", value)
+    }
+
+    update(delta) {
+        this.x = 5
+        this.y = 15
+    }
 }
