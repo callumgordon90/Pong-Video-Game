58b4a69fb958fe019d7fc8dc446858736135da23
callumgordon90 <callumjosephiaingordon@gmail.com>
2022-06-29 19:14:50 +0200

    corrected the bug with the computer paddle

--- Paddle.js
+++ Paddle.js
@@ -1,33 +1,32 @@
 //speed limit of computer paddle to make sure it isnt invincible:
-const SPEED = 0.2
+const SPEED = 0.02
 
 export default class Paddle {
-    constructor(paddleElem) {
-        this.paddleElem = paddleElem
-        this.reset()
-    }
+  constructor(paddleElem) {
+    this.paddleElem = paddleElem
+    this.reset()
+  }
 
-    get position() {
-        return parseFloat(getComputedStyle(this.paddleElem).getPropertyValue("--position"))
-    }
+  get position() {
+    return parseFloat(
+      getComputedStyle(this.paddleElem).getPropertyValue("--position")
+    )
+  }
 
-    set position(value) {
-        this.paddleElem.style.setProperty("--position", value)
-    }
+  set position(value) {
+    this.paddleElem.style.setProperty("--position", value)
+  }
 
-    rect() {
-        return this.paddleElem.getBoundingClientRect()
-    }
+  rect() {
+    return this.paddleElem.getBoundingClientRect()
+  }
 
-    reset() {
-        this.position = 50
-    }
+  reset() {
+    this.position = 50
+  }
 
-
-    //function to control computer paddle position:
-    update(delta, ballHeight) {
-        this.position += SPEED * delta * (ballHeight - this.position)
-      }
-
-
+  //function to control computer paddle position:
+  update(delta, ballHeight) {
+    this.position += SPEED * delta * (ballHeight - this.position)
+  }
 }
